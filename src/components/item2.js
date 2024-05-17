644da2bc611f315cf05ddc2af3d27091eda98375
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-05-17 18:11:12 +0000

    file

--- src/components/item2.js
+++ src/components/item2.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Item2 = () => {
     return (<div>
-        <h1>Item 1</h1>
-        <p>Description for Item 1</p>
+        <h1>Item 2</h1>
+        <p>Description for Item 2</p>
     </div>)
 }
 
--- src/components/item3.js
+++ src/components/item3.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Item3 = () => {
     return (<div>
-        <h1>Item 1</h1>
-        <p>Description for Item 1</p>
+        <h1>Item 3</h1>
+        <p>Description for Item 3</p>
     </div>)
 }
 
