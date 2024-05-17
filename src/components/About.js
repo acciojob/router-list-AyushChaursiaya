bddc96a0e453502606fc7ce326e41c61c55bc55e
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-05-17 18:20:27 +0000

    updated code

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 
 const About = () => {
     return (<div>
-        <h1>Item list</h1>
+        <h1>Item List</h1>
         <ul>
             <li>
                 <Link to='item1'>Item 1</Link>
